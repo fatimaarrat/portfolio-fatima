825620694e7066e5a1d7caa126c5590e20aa7240
Fatima Arrat <fatimaaraat@gmail.com>
2026-05-31 19:28:47 +0300

    add project2

--- script.js
+++ script.js
@@ -132,11 +132,11 @@ const activePortfolio1 = () => {
 };
 
 arrowRight1.addEventListener("click", () => {
-  if (indexa < 8) {
+  if (indexa < 10) {
     indexa++;
     arrowLeft1.classList.remove("disabled");
   } else {
-    indexa = 9;
+    indexa = 11;
     arrowRight1.classList.add("disabled");
   }
   activePortfolio1();
